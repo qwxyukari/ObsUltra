0bd0b08a19e0922f077f7508206f2022087ee3d8
qwxyukari <halseyistrefi@gmail.com>
2026-09-16 17:33:56 +0300

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -8450,8 +8450,6 @@ do
                 Transparency = 0.75,
                 Parent = Ball,
             })
-			Ball.Visible = false
-BallShadow.Visible = false
         end
 
         --// Pill shaped bar and fill, squaring off with everything else at radius 0
