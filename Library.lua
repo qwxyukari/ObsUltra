0ea561cb47d944789baa83ae108b91583a865664
qwxyukari <halseyistrefi@gmail.com>
2026-09-16 22:22:33 +0300

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -8281,7 +8281,7 @@ do
 
         local Holder = New("Frame", {
             BackgroundTransparency = 1,
-            Size = UDim2.new(1, 0, 0, Info.Compact and 15 or 33),
+            Size = UDim2.new(1, 0, 0, Info.Compact and 15 or 25),
             Visible = Slider.Visible,
             Parent = Container,
         })
