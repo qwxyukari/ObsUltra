85bb34d616d380ac7185649ee687da9ce7e02966
qwxyukari <halseyistrefi@gmail.com>
2026-09-16 21:59:06 +0300

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -8419,7 +8419,7 @@ do
 
             local FillWidthPx = Bar.AbsoluteSize.X * X
             local TextWidthPx = DisplayLabel.AbsoluteSize.X
-            local FitsOnLeft = FillWidthPx >= TextWidthPx + 6
+            local FitsOnLeft = FillWidthPx >= TextWidthPx + 4
 
             if FitsOnLeft then
                 DisplayLabel.AnchorPoint = Vector2.new(1, 0.5)
