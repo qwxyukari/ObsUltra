f27b78da3da05379b8a4e3db8d85e43efe59c083
qwxyukari <halseyistrefi@gmail.com>
2026-09-16 18:58:48 +0300

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -8325,10 +8325,10 @@ do
             Parent = Holder,
         })
 		New("UIPadding", {
-		PaddingBottom = UDim.new(0, 3),
-		PaddingTop = UDim.new(0, 3),
-		PaddingLeft = UDim.new(0, 3),
-		PaddingRight = UDim.new(0, 3),
+		PaddingBottom = UDim.new(0, 4),
+		PaddingTop = UDim.new(0, 4),
+		PaddingLeft = UDim.new(0, 4),
+		PaddingRight = UDim.new(0, 4),
 		Parent = Bar,
 		})
 
