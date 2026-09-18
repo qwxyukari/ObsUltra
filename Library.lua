5a7b95f127b0eb62f49cf09ccc57277f591e8aba
qwxyukari <halseyistrefi@gmail.com>
2026-09-18 14:21:53 +0300

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -14932,7 +14932,7 @@ function Library:CreateWindow(WindowInfo)
         })
         Library:MakeLine(MainFrame, {
             AnchorPoint = Vector2.new(0, 1),
-            Position = UDim2.new(0, 0, 1, -20),
+            Position = UDim2.new(0, 0, 1, -(20 + WindowInfo.CornerRadius)),
             Size = UDim2.new(1, 0, 0, 1),
         })
 
