0fc10c21c8602cb724271c97cb1329e38a02abc3
qwxyukari <halseyistrefi@gmail.com>
2026-09-20 22:06:09 +0300

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -14162,7 +14162,7 @@ function Library:CreateWindow(WindowInfo)
             BackgroundTransparency = 1,
             BorderSizePixel = 0,
             Image = "rbxassetid://8508019876",
-            Position = UDim2.fromOffset(0.1, 0.9),
+            Position = UDim2.fromOffset(1, 0.9),
             Size = UDim2.new(1, -5, 0, 2),
             ZIndex = 5,
             Visible = (WindowInfo.RainbowBar and WindowInfo.RainbowBar.Enabled ~= false),
